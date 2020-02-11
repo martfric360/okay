b85aed7e60797e281a6f7a64fda783e44297d3ba
Martin Frichitthavong <martfric360@ed.amdsb.ca>
2020-02-11 12:32:42 -0500

    Add project files.

---
+++ okay/VariablesPractice.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace okay
+{
+    partial class VariablesPractice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VariablesPractice));
+            this.Hockey = new System.Windows.Forms.Button();
+            this.Area = new System.Windows.Forms.Button();
+            this.Carpet = new System.Windows.Forms.Button();
+            this.Bill = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Hockey
+            // 
+            this.Hockey.Location = new System.Drawing.Point(12, 12);
+            this.Hockey.Name = "Hockey";
+            this.Hockey.Size = new System.Drawing.Size(140, 88);
+            this.Hockey.TabIndex = 0;
+            this.Hockey.Text = "Hockey";
+            this.Hockey.UseVisualStyleBackColor = true;
+            this.Hockey.Click += new System.EventHandler(this.Hockey_Click);
+            // 
+            // Area
+            // 
+            this.Area.Location = new System.Drawing.Point(12, 126);
+            this.Area.Name = "Area";
+            this.Area.Size = new System.Drawing.Size(140, 84);
+            this.Area.TabIndex = 1;
+            this.Area.Text = "Area";
+            this.Area.UseVisualStyleBackColor = true;
+            this.Area.Click += new System.EventHandler(this.Area_Click);
+            // 
+            // Carpet
+            // 
+            this.Carpet.Location = new System.Drawing.Point(13, 238);
+            this.Carpet.Name = "Carpet";
+            this.Carpet.Size = new System.Drawing.Size(141, 88);
+            this.Carpet.TabIndex = 2;
+            this.Carpet.Text = "Carpet";
+            this.Carpet.UseVisualStyleBackColor = true;
+            this.Carpet.Click += new System.EventHandler(this.Carpet_Click);
+            // 
+            // Bill
+            // 
+            this.Bill.Location = new System.Drawing.Point(13, 350);
+            this.Bill.Name = "Bill";
+            this.Bill.Size = new System.Drawing.Size(140, 88);
+            this.Bill.TabIndex = 3;
+            this.Bill.Text = "Bill";
+            this.Bill.UseVisualStyleBackColor = true;
+            this.Bill.Click += new System.EventHandler(this.Bill_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.BackColor = System.Drawing.SystemColors.Info;
+            this.outputLabel.Location = new System.Drawing.Point(175, 22);
+            this.outputLabel.MaximumSize = new System.Drawing.Size(1000000, 10000000);
+            this.outputLabel.MinimumSize = new System.Drawing.Size(590, 405);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(590, 405);
+            this.outputLabel.TabIndex = 4;
+            this.outputLabel.Text = "(Unkown)";
+            // 
+            // VariablesPractice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(782, 451);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.Bill);
+            this.Controls.Add(this.Carpet);
+            this.Controls.Add(this.Area);
+            this.Controls.Add(this.Hockey);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "VariablesPractice";
+            this.Text = "Variables Practice";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Hockey;
+        private System.Windows.Forms.Button Area;
+        private System.Windows.Forms.Button Carpet;
+        private System.Windows.Forms.Button Bill;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ okay/VariablesPractice.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace okay
+{
+    public partial class VariablesPractice : Form
+    {
+        public VariablesPractice()
+        {
+            InitializeComponent();
+            outputLabel.Text = "Waiting For Input..."; 
+            
+        }
+
+        private void Hockey_Click(object sender, EventArgs e)
+        {
+            int Number = 99; 
+            String Hockey = "WayneGretzky";
+            outputLabel.Text = Hockey + "' s Number is..." + Number; 
+        }
+
+        private void Area_Click(object sender, EventArgs e)
+        {
+            double radius = 15;
+            double pi = 3.14;
+            double area = radius * radius * pi;
+            outputLabel.Text = "The area of a circle with a 15 cm radius is..." + area + "cm^2"; 
+        }
+
+        private void Carpet_Click(object sender, EventArgs e)
+        {
+            double length = 8.5;
+            double width = 6;
+            double costPerMeter = 19.95;
+            double area = length * width;
+            double totalCost = length * width * costPerMeter;
+            outputLabel.Text = "The area of the room is..." + area + "m^2" + "\n" + "\n" + " The cost for carpet at 19.95 per square metre is ..." + totalCost + "$";
+           
+            
+            
+            
+
+        }
+
+        private void Bill_Click(object sender, EventArgs e)
+        {
+
+            double shirt = 12.49;
+            double tax = shirt * 0.13;
+            double total = shirt + tax;
+            double cash = 20;
+            double back = cash - total; 
+            outputLabel.Text = "Bill of Sale" + "\n" + "\n" + "Shirt:       " + shirt + "\n" + "\n" + "Tax:         " + tax + "\n" + "Total:       " + total + "\n" + "\n" + "Cash:        " + cash + "\n" + "Back:       " + back; 
+
+        }
+    }
+}
